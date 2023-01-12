2b1614571583da26ed845f7fc60974e8ae381ab4
Luis Felipe La Rotta <larottao@gmail.com>
2023-01-11 21:10:45 -0600

    Change Console.WriteLine to Debug.WriteLine

--- LaRottaO.CSharp.SoapUtilities/UtilsSoap.cs
+++ LaRottaO.CSharp.SoapUtilities/UtilsSoap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -23,109 +24,102 @@ namespace LaRottaO.CSharp.SoapUtilities
 
         public async Task<SoapResponse> executeRequest(String argEndpointUrl, List<String[]> argHeadersList, StringBuilder argXmlRequestBody, Boolean argIncludeRequestOnResponse = true, String argHttpMethod = "POST", int argTimeout = 40000, Boolean showDebug = true)
         {
-          
-                StringBuilder additionalData = new StringBuilder();
+            StringBuilder additionalData = new StringBuilder();
 
-                HttpWebResponse myHttpWebResponse = null;
-                StreamReader readStream = null;
+            HttpWebResponse myHttpWebResponse = null;
+            StreamReader readStream = null;
 
-                try
+            try
+            {
+                Tuple<Boolean, String, XmlDocument> xmlEnelopeCreationResult = CreateSoapEnvelope(argXmlRequestBody.ToString());
+
+                if (!xmlEnelopeCreationResult.Item1)
                 {
-                    Tuple<Boolean, String, XmlDocument> xmlEnelopeCreationResult = CreateSoapEnvelope(argXmlRequestBody.ToString());
-
-                    if (!xmlEnelopeCreationResult.Item1)
-                    {
-                        return new SoapResponse(false, 400, xmlEnelopeCreationResult.Item2);
-                    }
-
-                    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(argEndpointUrl);
-
-                    /*******************************************************************/
-                    // Headers by default
-                    /*******************************************************************/
-
-                    httpWebRequest.ContentType = "text/xml;charset=\"utf-8\"";
-                    httpWebRequest.Accept = "text/xml";
-                    httpWebRequest.Method = argHttpMethod;
-
-                    httpWebRequest.CookieContainer = new CookieContainer();
-                    httpWebRequest.KeepAlive = true;
-                    httpWebRequest.Timeout = argTimeout;
-
-                    /*******************************************************************/
-                    // Additional headers the user may want to add
-                    /*******************************************************************/
-
-                    foreach (String[] header in argHeadersList)
-                    {
-                        additionalData.Append(additionalData + header[0] + ":" + header[1] + Environment.NewLine);
-                        httpWebRequest.Headers.Add(header[0], header[1]);
-                    }
-
-                    Tuple<Boolean, String> insertSoapEnvelopeResult = InsertSoapEnvelopeIntoWebRequest(xmlEnelopeCreationResult.Item3, httpWebRequest);
-
-                    if (!insertSoapEnvelopeResult.Item1)
-                    {
-                        return new SoapResponse(false, 500, insertSoapEnvelopeResult.Item2);
-                    }
-                    
-                 
-                    additionalData.Append(argXmlRequestBody.ToString() + Environment.NewLine + Environment.NewLine);
-               
-
-                    myHttpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-                    if (myHttpWebResponse.StatusCode != HttpStatusCode.OK)
-                    {
-                        return new SoapResponse(false, 500, additionalData + myHttpWebResponse.StatusDescription);
-                    }
-
-                    StringBuilder responseBody = new StringBuilder();
-            
-                    Stream receiveStream = myHttpWebResponse.GetResponseStream();
-                    Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
-              
-                    readStream = new StreamReader(receiveStream, encode);
-
-                    Char[] read = new Char[256];
-
-               
-                    int count = readStream.Read(read, 0, 256);
-
-                    while (count > 0)
-                    {                        
-
-                        String str = new String(read, 0, count);
-                        responseBody.Append(str);
-                        count = readStream.Read(read, 0, 256);
-                    }
-
-                    if (showDebug)
-                    {
-                        Console.WriteLine(additionalData + responseBody.ToString());
-                    }
-
-                    return new SoapResponse(true, 200, additionalData + responseBody.ToString());
+                    return new SoapResponse(false, 400, xmlEnelopeCreationResult.Item2);
                 }
-                catch (Exception e)
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(argEndpointUrl);
+
+                /*******************************************************************/
+                // Headers by default
+                /*******************************************************************/
+
+                httpWebRequest.ContentType = "text/xml;charset=\"utf-8\"";
+                httpWebRequest.Accept = "text/xml";
+                httpWebRequest.Method = argHttpMethod;
+
+                httpWebRequest.CookieContainer = new CookieContainer();
+                httpWebRequest.KeepAlive = true;
+                httpWebRequest.Timeout = argTimeout;
+
+                /*******************************************************************/
+                // Additional headers the user may want to add
+                /*******************************************************************/
+
+                foreach (String[] header in argHeadersList)
                 {
-                    Console.WriteLine(additionalData + " " + e.Message);
-                    return new SoapResponse(true, 401, additionalData + " " + e.Message);
+                    additionalData.Append(additionalData + header[0] + ":" + header[1] + Environment.NewLine);
+                    httpWebRequest.Headers.Add(header[0], header[1]);
                 }
-                finally
-                {                 
 
-                    if (myHttpWebResponse != null)
-                    {
-                        myHttpWebResponse.Close();
-                    }                                      
+                Tuple<Boolean, String> insertSoapEnvelopeResult = InsertSoapEnvelopeIntoWebRequest(xmlEnelopeCreationResult.Item3, httpWebRequest);
 
-                    if (readStream != null)
-                    {
-                        readStream.Close();
-                    }
+                if (!insertSoapEnvelopeResult.Item1)
+                {
+                    return new SoapResponse(false, 500, insertSoapEnvelopeResult.Item2);
                 }
-           
+
+                additionalData.Append(argXmlRequestBody.ToString() + Environment.NewLine + Environment.NewLine);
+
+                myHttpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                if (myHttpWebResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    return new SoapResponse(false, 500, additionalData + myHttpWebResponse.StatusDescription);
+                }
+
+                StringBuilder responseBody = new StringBuilder();
+
+                Stream receiveStream = myHttpWebResponse.GetResponseStream();
+                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+
+                readStream = new StreamReader(receiveStream, encode);
+
+                Char[] read = new Char[256];
+
+                int count = readStream.Read(read, 0, 256);
+
+                while (count > 0)
+                {
+                    String str = new String(read, 0, count);
+                    responseBody.Append(str);
+                    count = readStream.Read(read, 0, 256);
+                }
+
+                if (showDebug)
+                {
+                    Debug.WriteLine(additionalData + responseBody.ToString());
+                }
+
+                return new SoapResponse(true, 200, additionalData + responseBody.ToString());
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(additionalData + " " + e.Message);
+                return new SoapResponse(true, 401, additionalData + " " + e.Message);
+            }
+            finally
+            {
+                if (myHttpWebResponse != null)
+                {
+                    myHttpWebResponse.Close();
+                }
+
+                if (readStream != null)
+                {
+                    readStream.Close();
+                }
+            }
         }
 
         private static Tuple<Boolean, String, XmlDocument> CreateSoapEnvelope(String textoXmlRequest)
@@ -153,7 +147,6 @@ namespace LaRottaO.CSharp.SoapUtilities
 
                 return new Tuple<Boolean, String>(true, "");
             }
-
             catch (Exception ex)
             {
                 return new Tuple<Boolean, String>(false, "Unable to Insert SOAP envelope into Web Request: " + ex.ToString());
